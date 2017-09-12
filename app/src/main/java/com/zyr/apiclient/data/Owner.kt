0b968e4124e2d1dd999f24af01ed5afd441841e9
YuranZhang <371927983@qq.com>
2017-09-12 17:59:45 +0800

    Add data classes

---
+++ app/src/main/java/com/zyr/apiclient/data/Owner.kt
@@ -0,0 +1,22 @@
+package com.zyr.apiclient.data
+
+
+data class Owner(
+		var login: String,
+		var id: Int,
+		var avatar_url: String,
+		var gravatar_id: String,
+		var url: String,
+		var html_url: String,
+		var followers_url: String,
+		var following_url: String,
+		var gists_url: String,
+		var starred_url: String,
+		var subscriptions_url: String,
+		var organizations_url: String,
+		var repos_url: String,
+		var events_url: String,
+		var received_events_url: String,
+		var type: String,
+		var site_admin: Boolean
+)
